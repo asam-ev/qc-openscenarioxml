525a6456dc4819d61130c7fcf8466d27b33a4766
Ben Engel <benjamin.engel@asam.net>
2024-01-19 16:07:34 +0100

    initial commit

---
+++ checks/schema_checks/check_conventions.py
@@ -0,0 +1,17 @@
+from checker_data import CheckerData
+
+
+def check_catalogs(checker_data: CheckerData) -> None:
+    pass
+
+
+def get_checker_id():
+    return 'ConventionChecker'
+
+
+def get_description():
+    return 'Checks conventions in OSC file.'
+
+
+def check(checker_data: CheckerData) ->None:
+    check_catalogs(checker_data)
---
+++ checks/schema_checks/schema/schema_files.py
@@ -0,0 +1,5 @@
+SCHEMA_FILES = {
+    '0.9': ['0.9.1/OpenSCENARIO_v0.9.1.xsd'],
+    '1.0': ['1.0.0/OpenSCENARIO.xsd'],
+    '1.1': ['1.1.0/OpenSCENARIO.xsd']
+}
