01d77e99ab26bc48686d9e54a215e8c9b5c01236
romanodanilo <62891297+romanodanilo@users.noreply.github.com>
2024-06-19 17:29:01 +0200

    Add unique name checks and tests (#11)
    Signed-off-by: romanodanilo <danilo@ivex.ai>
    
    Signed-off-by: romanodanilo <62891297+romanodanilo@users.noreply.github.com>

--- qc_openscenario/checks/utils.py
+++ qc_openscenario/checks/utils.py
@@ -9,3 +9,35 @@ def get_standard_schema_version(root: etree._ElementTree) -> Union[str, None]:
     header_attrib = header.attrib
     version = f"{header_attrib['revMajor']}.{header_attrib['revMinor']}.0"
     return version
+
+
+def compare_versions(version1: str, version2: str) -> int:
+    """Compare two version strings like "X.x.x"
+        This function is to avoid comparing version string basing on lexicographical order
+        that could cause problem. E.g.
+        1.10.0 > 1.2.0 but lexicographical comparison of string would return the opposite
+
+    Args:
+        version1 (str): First string to compare
+        version2 (str): Second string to compare
+
+    Returns:
+        int: 1 if version1 is bigger than version2. 0 if the version are the same. -1 otherwise
+    """
+    v1_components = list(map(int, version1.split(".")))
+    v2_components = list(map(int, version2.split(".")))
+
+    # Compare each component until one is greater or they are equal
+    for v1, v2 in zip(v1_components, v2_components):
+        if v1 < v2:
+            return -1
+        elif v1 > v2:
+            return 1
+
+    # If all components are equal, compare based on length
+    if len(v1_components) < len(v2_components):
+        return -1
+    elif len(v1_components) > len(v2_components):
+        return 1
+    else:
+        return 0
